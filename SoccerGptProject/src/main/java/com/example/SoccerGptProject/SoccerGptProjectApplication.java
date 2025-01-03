4fae291f870d97a5e6511b5df1b9b5c18cf704bb
parkhss <115133766+parkhss@users.noreply.github.com>
2025-01-03 20:05:12 +0900

    SpringBoot 생성

---
+++ SoccerGptProject/src/main/java/com/example/SoccerGptProject/SoccerGptProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.SoccerGptProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerGptProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoccerGptProjectApplication.class, args);
+	}
+
+}
---
+++ SoccerGptProject/src/test/java/com/example/SoccerGptProject/SoccerGptProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SoccerGptProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoccerGptProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
