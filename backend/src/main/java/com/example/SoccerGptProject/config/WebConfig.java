94465faa7d2996a5dd42c83339d456af48e40419
parkhss <115133766+parkhss@users.noreply.github.com>
2025-01-07 21:36:44 +0900

    Cors 설정, react 코드 추가

---
+++ backend/src/main/java/com/example/SoccerGptProject/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.example.SoccerGptProject.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 엔드포인트에 cors 설정 적용
+                .allowedOrigins("http://localhost:3000") // 허용할 출처
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP method
+                .allowCredentials(true) // 쿠키 인증 요청 허용
+                .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
+    }
+}
+
+
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from "react"; 
+
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    // Spring Boot API 호출
+    fetch("http://localhost:8080/api/v1/testApi/hello") // Spring Boot 서버 주소
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.text(); // 응답을 텍스트로 변환
+      })
+      .then((data) => setMessage(data)) // 상태에 저장
+      .catch((error) => console.error("Fetch error:", error));
+  }, []); // 빈 배열로 한 번만
+
+
   return (
     <div className="App">
       <header className="App-header">
+        <h1>API 호출 결과 입니다 : {message}</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
